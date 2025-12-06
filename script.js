d30b60c517cdc8d84b8eb80bc30e5a57a7de40b3
ArpitaAmbalkar <arpitaambalkar26@gmail.com>
2025-12-06 13:37:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ const wins = [
 
 submitBtn.addEventListener('click', () => {
 
-  player1 = document.getElementById('player-1').value || "Player 1";
-  player2 = document.getElementById('player-2').value || "Player 2";
+  player1 = document.getElementById('player1').value || "Player 1";
+  player2 = document.getElementById('player2').value || "Player 2";
   currentPlayer = player1;
 
   message.textContent = `${currentPlayer}, you're up!`;
